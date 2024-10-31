9907cdd657dde8aa7c476ea74552b1f3f1a56a1a
zintechideas <93466110+zintechideas@users.noreply.github.com>
2024-10-31 19:58:10 +0330

    Add files via upload

---
+++ Analog Read.py
@@ -0,0 +1,11 @@
+from machine import Pin, ADC
+from time import sleep
+
+pot = ADC(Pin(4))
+pot.atten(ADC.ATTN_11DB)       #Full range: 3.3v
+
+while True:
+  pot_value = pot.read()
+  print(pot_value)
+  sleep(0.1)
+
